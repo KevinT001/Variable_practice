86a0934dfcd1d238605e98a1d311049e1395371e
Kevin Ta <kvnta@yahoo.com>
2022-03-29 20:28:53 -0600

    Annotate three lines

--- variables.rb
+++ variables.rb
@@ -1,7 +1,7 @@
 flower = "rose"
 dog = "corgi"
 beer = "stout"
-
+#integer variables defined below
 flower_number = 10
 dog_age = 9
 beers_drank = 7
@@ -9,12 +9,13 @@ beers_drank = 7
 price_of_rose = 5.99
 dog_bill = 569.32
 alcohol_percentage = 5.60
-
+#Boolean variables
 flower_has_thorns = true
 dog_is_spayed = false
 beer_is_domestic = true
 
 
+# Reassign variables for some variables
 flower = "sunflower"
 beers_drank = 12
 flower_has_thorns = false
